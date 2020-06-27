808ea3fff0322e41534ce9d993f3aa4c9c35721a
Osama <osamanepral@gmail.com>
2020-06-27 17:28:35 +0300

    Upload Pstr now

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from distutils.core import setup
 setup(name='pstr',
       version='1.0.3',
-      description='Python script for terminal text/string',
+      description='Python Module for terminal text/string',
       author='Osama Muhammed',
       author_email='osamanepral@gmail.com',
       url='https://www.pyosama.heroku.com',
